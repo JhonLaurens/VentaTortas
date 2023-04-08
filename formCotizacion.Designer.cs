55e8a1818ac42b89f9392099f8321bdffc029ad0
JhonLaurens <jhonlaurens@gmail.com>
2023-04-08 02:12:03 -0500

    COTIZACION DE PRODUCTOS PENDIENTE EDITAR DISEÑO FORMULARIO

--- formCotizacion.Designer.cs
+++ formCotizacion.Designer.cs
@@ -45,7 +45,12 @@
             this.btnGuardar = new System.Windows.Forms.Button();
             this.dgPantalla = new System.Windows.Forms.DataGridView();
             this.mtHoraFecha = new System.Windows.Forms.MaskedTextBox();
+            this.dgProductos = new System.Windows.Forms.DataGridView();
+            this.dgSeleccionados = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgPantalla)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSeleccionados)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSalir2
@@ -214,9 +219,9 @@
             // dgPantalla
             // 
             this.dgPantalla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgPantalla.Location = new System.Drawing.Point(357, 106);
+            this.dgPantalla.Location = new System.Drawing.Point(333, 40);
             this.dgPantalla.Name = "dgPantalla";
-            this.dgPantalla.Size = new System.Drawing.Size(422, 231);
+            this.dgPantalla.Size = new System.Drawing.Size(446, 105);
             this.dgPantalla.TabIndex = 24;
             // 
             // mtHoraFecha
@@ -228,12 +233,43 @@
             this.mtHoraFecha.TabIndex = 14;
             this.mtHoraFecha.ValidatingType = typeof(System.DateTime);
             // 
+            // dgProductos
+            // 
+            this.dgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProductos.Location = new System.Drawing.Point(333, 160);
+            this.dgProductos.Name = "dgProductos";
+            this.dgProductos.Size = new System.Drawing.Size(446, 183);
+            this.dgProductos.TabIndex = 25;
+            // 
+            // dgSeleccionados
+            // 
+            this.dgSeleccionados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSeleccionados.Location = new System.Drawing.Point(333, 400);
+            this.dgSeleccionados.Name = "dgSeleccionados";
+            this.dgSeleccionados.ShowCellErrors = false;
+            this.dgSeleccionados.ShowRowErrors = false;
+            this.dgSeleccionados.Size = new System.Drawing.Size(450, 158);
+            this.dgSeleccionados.TabIndex = 26;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(333, 354);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 28);
+            this.btnAgregar.TabIndex = 27;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
             // formCotizacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnSalir2;
-            this.ClientSize = new System.Drawing.Size(816, 384);
+            this.ClientSize = new System.Drawing.Size(821, 598);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgSeleccionados);
+            this.Controls.Add(this.dgProductos);
             this.Controls.Add(this.mtHoraFecha);
             this.Controls.Add(this.dgPantalla);
             this.Controls.Add(this.btnGuardar);
@@ -259,6 +295,8 @@
             this.Text = "Cotizacion";
             this.Load += new System.EventHandler(this.formCotizacion_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgPantalla)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSeleccionados)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -283,5 +321,8 @@
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.DataGridView dgPantalla;
         private System.Windows.Forms.MaskedTextBox mtHoraFecha;
+        private System.Windows.Forms.DataGridView dgProductos;
+        private System.Windows.Forms.DataGridView dgSeleccionados;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
--- formCotizacion.cs
+++ formCotizacion.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace VentaTortas
@@ -9,7 +11,37 @@ namespace VentaTortas
 
 
     {
+        private void formCotizacion_Load(object sender, EventArgs e)
+        {
+            // Agregar las columnas del DataGridView
+            dgProductos.Columns.Add("Id", "Id");
+            dgProductos.Columns.Add("Producto", "Producto");
+            dgProductos.Columns.Add("Sabor", "Sabor");
+            dgProductos.Columns.Add("Porciones", "No. de porciones");
+            dgProductos.Columns.Add("Valor", "Valor unitario");
+            dgProductos.Columns.Add("Cantidad", "Cantidad");
 
+
+            // Agregar las filas de la tabla
+            dgProductos.Rows.Add("1", "Torta1", "Naranja", "20", "$50,000", 1);
+            dgProductos.Rows.Add("2", "Torta2", "Naranja", "10", "$30,000", 1);
+            dgProductos.Rows.Add("3", "Torta3", "Chocolate", "10", "$35,000", 1);
+            dgProductos.Rows.Add("4", "Torta4", "Chocolate", "20", "$70,000", 1);
+            dgProductos.Rows.Add("5", "Torta5", "Red Velvet", "10", "$40,000", 1);
+            dgProductos.Rows.Add("6", "Torta6", "Red Velvet", "20", "$80,000", 1);
+        }
+
+        public class Producto
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Sabor { get; set; }
+            public string Porciones { get; set; }
+            public decimal Valor { get; set; }
+            public int Cantidad { get; set; }
+        }
+        // Declarar la lista de clientes
+        List<Producto> listaProductosSeleccionados = new List<Producto>();
 
         public class Cliente
         {
@@ -20,6 +52,8 @@ namespace VentaTortas
             public string Ciudad { get; set; }
             public string Direccion { get; set; }
             public DateTime Hora_Fecha_Envio { get; set; }
+            public string Precio_Total { get; set; }
+
         }
 
         // Declarar la lista de clientes
@@ -32,20 +66,93 @@ namespace VentaTortas
             this.btnGuardar = btnGuardar;
         }
 
-        private void formCotizacion_Load(object sender, EventArgs e)
-        {
 
-        }
 
         private void btnSalir_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Mostrar un mensaje al usuario para preguntar qué producto desea agregar
+            int id = 0;
+            bool productoValido = false;
+            while (!productoValido)
+            {
+                string input = Microsoft.VisualBasic.Interaction.InputBox("Ingrese el ID del producto que desea agregar:", "Agregar producto", "");
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    // El usuario canceló la operación
+                    return;
+                }
+                if (!int.TryParse(input, out id))
+                {
+                    // El usuario ingresó un valor no numérico
+                    MessageBox.Show("Por favor ingrese un valor numérico para el ID del producto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    // Buscar el producto con el ID ingresado
+                    DataGridViewRow row = dgProductos.Rows.Cast<DataGridViewRow>().FirstOrDefault(r => r.Cells["Id"].Value.ToString() == id.ToString());
+                    if (row == null)
+                    {
+                        // No se encontró el producto con el ID ingresado
+                        MessageBox.Show($"No se encontró ningún producto con el ID {id}. Por favor ingrese un ID válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        // El producto con el ID ingresado fue encontrado
+                        productoValido = true;
+                    }
+                }
+            }
+
+            // Obtener la cantidad seleccionada por el usuario
+            DataGridViewRow productoRow = dgProductos.Rows.Cast<DataGridViewRow>().FirstOrDefault(r => r.Cells["Id"].Value.ToString() == id.ToString());
+            int cantidad = Convert.ToInt32(productoRow.Cells["Cantidad"].Value);
+
+            // Si la cantidad es mayor a cero, agregar el producto a la lista de productos seleccionados
+            if (cantidad > 0)
+            {
+                // Crear un objeto que represente el producto seleccionado
+                Producto producto = new Producto();
+                producto.Id = id;
+                producto.Nombre = productoRow.Cells["Producto"].Value.ToString();
+                producto.Sabor = productoRow.Cells["Sabor"].Value.ToString();
+                producto.Porciones = productoRow.Cells["Porciones"].Value.ToString(); ;
+                producto.Valor = Convert.ToDecimal(productoRow.Cells["Valor"].Value.ToString().Replace("$", "").Replace(",", ""));
+                producto.Cantidad = cantidad;
+
+
+                // Agregar el producto a la lista de productos seleccionados
+                listaProductosSeleccionados.Add(producto);
+
+                // Mostrar un mensaje al usuario confirmando que el producto fue agregado
+                MessageBox.Show($"El producto {producto.Nombre} ({producto.Sabor}) con ID {producto.Id} y {producto.Porciones} porción(es) ha sido agregado.", "Producto agregado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Mostrar la lista de productos seleccionados en el DataGridView
+            dgSeleccionados.DataSource = null;
+            dgSeleccionados.DataSource = listaProductosSeleccionados;
+            
+        }
+
         private void btnGuardar_Click(object sender, EventArgs e)
         {
+            // Calcular el precio total de los productos seleccionados
+            decimal precioTotal = 0;
+            foreach (Producto producto in listaProductosSeleccionados)
+            {
+
+
+                precioTotal += producto.Valor;
+            } 
 
             Cliente nuevoCliente = new Cliente();
+
+
 
             nuevoCliente.Nombres = txtNombre.Text;
             nuevoCliente.Apellidos = txtApellidos.Text;
@@ -53,12 +160,16 @@ namespace VentaTortas
             nuevoCliente.Email = txtEmail.Text;
             nuevoCliente.Ciudad = cmbCiudad.SelectedItem.ToString();
             nuevoCliente.Direccion = txtDireccion.Text;
+           
             nuevoCliente.Hora_Fecha_Envio = DateTime.ParseExact(mtHoraFecha.Text, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            nuevoCliente.Precio_Total = "$ " + precioTotal.ToString("N0");
 
-            // Obtener la hora a partir de
-            //
-            // la cadena del tiempo
 
+
+            // Mostrar el precio total en un MessageBox
+            MessageBox.Show("Precio total: $" + precioTotal.ToString("N0"));
+
+           
             listaClientes.Add(nuevoCliente);
 
             txtNombre.Clear();
@@ -68,15 +179,15 @@ namespace VentaTortas
             cmbCiudad.SelectedIndex = -1;
             txtDireccion.Clear();
             mtHoraFecha.Clear();
+            listaProductosSeleccionados.Clear();
 
             // Mostrar la lista de clientes en el DataGridView llamado dgPantalla
             dgPantalla.DataSource = null;
             dgPantalla.DataSource = listaClientes;
 
-
+           
         }
 
-
-
+       
     }
 }
